54cd4467f44292b508381aca7c0ec0815160d088
RoyalSoft <royalsoftkv@gmail.com>
2020-12-12 13:44:37 +0100

    Added install script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const minimist = require('minimist')
 
 let argv = minimist(process.argv.slice(2));
 if(argv._.length === 0) {
-    console.info('Usage: ncfc (server|send|receive|version) [options]')
+    console.info('Usage: ncfc (server|send|receive|version|install) [options]')
     process.exit()
 }
 
@@ -18,6 +18,20 @@ if(action === 'server') {
     let pjson = require('./package.json')
     console.info(pjson.version)
     process.exit()
+} else if (action === 'install') {
+    const fs = require('fs')
+    const path = require('path')
+    fs.mkdirSync(path.join(process.cwd(), 'bin'),{ recursive: true })
+    let file = path.join(process.cwd(), 'bin', 'ncfc')
+    let content = `#/bin/bash
+node ${process.cwd()}/index.js "$@"`
+    fs.writeFileSync(file, content)
+    fs.chmodSync(file, 0o755)
+    let binFile = '/usr/bin/ncfc'
+    if(fs.existsSync(binFile)){
+        fs.unlinkSync(binFile)
+    }
+    fs.symlinkSync(file, binFile)
 } else {
     console.error(`Invalid action ${action}`)
     console.info('Usage: ncfc (server|send|receive) [options]')
